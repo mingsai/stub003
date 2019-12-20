dc68bb4b116d01f9b44431feff441dd611443f96
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-12-19 17:42:21 -0800

    init

---
+++ linux/flutter/ephemeral/generated_config.mk
@@ -0,0 +1,5 @@
+# Generated code do not commit.
+export FLUTTER_ROOT=/opt/flutter
+export TRACK_WIDGET_CREATION=true
+export FLUTTER_TARGET=/home/tcarternyc/FlutterProjects/helloworld/lib/main.dart
+export PROJECT_DIR=/home/tcarternyc/FlutterProjects/helloworld
